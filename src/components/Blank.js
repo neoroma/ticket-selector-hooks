7c2c6389b2d7d248723ef1ed810f53ef62df2a05
Roman Shestakov <neoroma@gmail.com>
2019-03-27 18:03:12 +0200

    Add transitions to the blank page

--- src/components/Blank.js
+++ src/components/Blank.js
@@ -4,13 +4,37 @@ import styled from 'styled-components'
 const Wrap = styled.section`
   width: 75vw;
   height: 75vh;
-  background: violet;
+  background: LIGHTYELLOW;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const OnTransition = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: crimson;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all .3s ease-out;
+  cursor: pointer;
+  border-radius: 50%;
+  
+  :hover {
+    background-color: orangered;
+    height: 105px;
+    width: 105px;
+  }
 `
 
 function Blank() {
   return (
       <Wrap>
-        Blank
+        <OnTransition>
+          cube
+        </OnTransition>
       </Wrap>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import Root from './components/Root'
-import Landing from './components/Landing'
+import Root from './components/Root'
+// import Landing from './components/Landing'
 
-ReactDOM.render(<Landing />, document.getElementById('root'))
+ReactDOM.render(<Root />, document.getElementById('root'))
